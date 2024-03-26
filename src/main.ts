078386a7669c17442379c9eca261bf30a3b85f86
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-03-26 18:15:03 +0800

    feat: ping command

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { DaphneClient } from "@/extensions/daphne-client";
 import { configuration } from "./configuration";
 import { env } from "./env";
 
+/**
+ * The main entry point for the bot.
+ * @see DaphneClient
+ */
 const main = async (): Promise<void> => {
     void new DaphneClient(configuration).login(env.DISCORD_BOT_TOKEN);
 };
